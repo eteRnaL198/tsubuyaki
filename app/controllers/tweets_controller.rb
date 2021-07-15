098c18e57afae682aa34ebe86f59285fc73168ba
eteRnaL198 <ikuya313198@gmail.com>
2021-07-15 19:44:57 +0900

    update

---
+++ app/controllers/tweets_controller.rb
@@ -0,0 +1,33 @@
+class TweetsController < ApplicationController
+  before_action :set_tweet, only [:edit, :show]
+
+  def index
+    @tweets = Tweet.all
+  end
+
+  def new
+    @tweet = Tweet.new
+  end
+
+  def create
+    Tweet.create(tweet_params)
+  end
+
+  def destroy
+    tweet = Tweet.find(params[:id])
+    tweet.destroy
+  end
+
+  def edit
+  end
+
+  def update
+    tweet = Tweet.find(params[:id])
+    tweet.update(tweet_params)
+  end
+
+  private
+  def tweet_params
+    params.require(:tweet).permit(:name, :text)
+  end
+end
